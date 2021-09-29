8a735a34c6eeb19563f36a3669de567fcd17c129
OzonoYui <yuimaaru0430@yahoo.co.jp>
2021-09-29 20:49:13 +0900

    チーム開発機能

---
+++ PurchaseHistoryController.java
@@ -0,0 +1,46 @@
+package jp.co.internous.twinkle.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import jp.co.internous.twinkle.model.domain.dto.PurchaseHistoryDto;
+import jp.co.internous.twinkle.model.mapper.TblPurchaseHistoryMapper;
+import jp.co.internous.twinkle.model.session.LoginSession;
+
+@Controller
+@RequestMapping("/twinkle/history")
+public class PurchaseHistoryController {
+	
+	@Autowired
+	private LoginSession loginSession;
+	
+	@Autowired
+	private TblPurchaseHistoryMapper purchaseHistoryMapper;
+	
+	@RequestMapping("/")
+	public String index(Model m) {
+		int userId=loginSession.getUserId();
+		List<PurchaseHistoryDto> historyList=purchaseHistoryMapper.findByUserId(userId);
+		
+		m.addAttribute("historyList",historyList);
+		m.addAttribute("loginSession",loginSession);
+		
+		return "purchase_history";
+		
+	}
+	
+	@RequestMapping("/delete")
+	@ResponseBody
+	public boolean delete() {
+		int userId=loginSession.getUserId();
+		int result=purchaseHistoryMapper.logicalDeleteByUserId(userId);
+		
+		return result > 0;
+		
+	}
+}
